a901af27ec121fb5eb74fc76f021996812f5b020
tzx6 <tzx6@cdc.gov>
2024-06-10 09:53:44 -0400

    bug: reactiveness too small for publications

--- script.js
+++ script.js
@@ -3,30 +3,6 @@ document.getElementById('darkModeToggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 });
 
-// Collapsible sections
-var coll = document.getElementsByClassName("collapsible");
-for (var i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
-}
-
-// Show publication details when an image is clicked
-function showDetails(id) {
-    var details = document.getElementById(id);
-    if (details.style.display === 'none' || details.style.display === '') {
-        details.style.display = 'block';
-    } else {
-        details.style.display = 'none';
-    }
-}
-
 // Tab functionality
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
